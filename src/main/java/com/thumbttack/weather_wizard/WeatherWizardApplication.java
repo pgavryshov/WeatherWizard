2ca665d3f1e07334a6c53cb3720c8ed03941a009
pgavryshov <pgavryshov@lineate.com>
2018-11-06 17:04:28 +0400

    add Spring Boot 2

---
+++ src/main/java/com/thumbttack/weather_wizard/WeatherWizardApplication.java
@@ -0,0 +1,12 @@
+package com.thumbttack.weather_wizard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherWizardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherWizardApplication.class, args);
+    }
+}
